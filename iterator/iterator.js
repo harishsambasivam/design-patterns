bc4dfbffb64aa30edf95f69b1b1a2c7a8176bc8d
harishsambasivam <imharishsambasivam@gmail.com>
2020-07-20 16:28:04 +0530

    added Iterator pattern

---
+++ iterator/iterator.js
@@ -0,0 +1,33 @@
+// https://medium.com/better-programming/understanding-the-iterator-pattern-in-javascript-typescript-using-symbol-iterator-ab400d46b14a
+
+class Iterator {
+  constructor(item) {
+    this.items = item;
+    this.index = 0;
+  }
+
+  hasNext() {
+    return this.index < this.items.length;
+  }
+
+  value() {
+    return this.items[this.index++];
+  }
+
+  next() {
+    return { value: this.value(), done: !this.hasNext() };
+  }
+}
+
+let iterator = new Iterator([1, 2, 3, 4, 5]);
+
+while (iterator.hasNext()) {
+  console.log(iterator.next());
+}
+
+// Output:
+// { value: 1, done: false }
+// { value: 2, done: false }
+// { value: 3, done: false }
+// { value: 4, done: false }
+// { value: 5, done: true }
